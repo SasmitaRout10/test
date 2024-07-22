5dcda7f29804bd0019699c718753a798a1d8cba4
SasmitaRout10 <sasmitarout@gmail.com>
2024-07-22 16:54:11 +0530

    Create streamlit_app.py
    copied from my custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write("""Orders that need to be filled.""")
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders") 
+editable_df = st.experimental_data_editor(my_dataframe)
+ 
+submitted = st.button('Submit')
+if submitted: 
+    st.success("Someone clicked the button.",icon="👍🏻")
+
+    og_dataset = session.table("smoothies.public.orders")
+    edited_dataset = session.create_dataframe(editable_df)
+    og_dataset.merge(edited_dataset
+                     , (og_dataset['name_on_order'] == edited_dataset['name_on_order'])
+                     ,  [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
